a2396ce9cf2cd712f1ae22ab92ec88c5b8483c78
Jaunty <jaunty5899@gmail.com>
2023-03-12 01:05:05 +0530

    v0.1

--- script.js
+++ script.js
@@ -1,8 +1,36 @@
-const input = document.querySelector('input');
-const mov_box = document.querySelector('.mov-list');
+const input = document.querySelector("input");
+const list_box = document.querySelector(".list-box");
+const mov_obj = "https://yts.mx/api/v2/list_movies.json?sort_by=year&limit=50";
 
-input.addEventListener('input',()=>{
-    mov_box.classList.remove('d-none');
-    if (input.value=='')
-        mov_box.classList.add('d-none');
-})
+input.addEventListener("keyup", async (e) => {
+  if (e.key == "Enter") {
+    const usr_input = input.value.toLowerCase();
+    // console.log(usr_input);
+    const result = await fetch(mov_obj).then((res) => {
+      return res.json();
+    });
+    // console.log(res);
+    let found_items = [],
+      j = 0;
+    for (let i of result.data.movies) {
+      if (i.title.toLowerCase().includes(usr_input)) {
+        found_items[j] = {
+          title: i.title,
+          year: i.year,
+          cover: i.small_cover_image,
+        };
+        j++;
+      }
+    }
+    // console.log(found_items);
+    // console.log(result.length);
+    list_box.innerHTML = "";
+    if (found_items.length == 0) {
+      list_box.innerHTML += `<li><span><i>NO RESULTS FOUND!</i></span></li>`;
+    } else {
+      for (let i = 0; i < found_items.length; i++) {
+        list_box.innerHTML += `<li><span><img src = '${found_items[i].cover}'><b><i>${found_items[i].year}</i></b></span><span>${found_items[i].title}</span></li>`;
+      }
+    }
+  }
+});
