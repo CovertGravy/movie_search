304ec081fc83ed641fdf120bc4fdf2aa3bfb2cf3
Jaunty <jaunty5899@gmail.com>
2023-03-12 16:11:45 +0530

    anim@li

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ const input = document.querySelector("input");
 const list_box = document.querySelector(".list-box");
 const mov_obj = "https://yts.mx/api/v2/list_movies.json?sort_by=year&limit=50";
 
-input.addEventListener("keyup", async (e) => {
-  if (e.key == "Enter") {
+input.addEventListener("input", async (e) => {
+  if (e.value == "") {
+    list_box.innerHTML = "";
+  } else {
     const usr_input = input.value.toLowerCase();
     // console.log(usr_input);
     const result = await fetch(mov_obj).then((res) => {
@@ -29,9 +31,15 @@ input.addEventListener("keyup", async (e) => {
       list_box.innerHTML += `<li><span><i>NO RESULTS FOUND!</i></span></li>`;
     } else {
       for (let i = 0; i < found_items.length; i++) {
+        list_box.innerHTML += `<li class="animate__animated d-none"><span><img src = '${found_items[i].cover}'><b><i>${found_items[i].year}</i></b></span><span>${found_items[i].title}</span></li>`;
+      }
+
+      const list_item = document.querySelectorAll("li");
+      for (let i = 0; i < list_item.length; i++) {
         setTimeout(() => {
-          list_box.innerHTML += `<li class="animate__animated animate__bounce"><span><img src = '${found_items[i].cover}'><b><i>${found_items[i].year}</i></b></span><span>${found_items[i].title}</span></li>`;
-        }, 2000);
+          list_item[i].classList.remove("d-none");
+          list_item[i].classList.add("animate__fadeInRight");
+        }, 1000 * i);
       }
     }
   }
