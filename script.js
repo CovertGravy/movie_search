9838a5d76dc29fac744033388b614f038304bc70
Jaunty <jaunty5899@gmail.com>
2023-03-15 13:16:03 +0530

    v0.2

--- script.js
+++ script.js
@@ -2,45 +2,39 @@ const input = document.querySelector("input");
 const list_box = document.querySelector(".list-box");
 const mov_obj = "https://yts.mx/api/v2/list_movies.json?sort_by=year&limit=50";
 
-input.addEventListener("input", async (e) => {
-  if (e.value == "") {
+input.addEventListener("input", (e) => {
+  if (input.value == "") {
     list_box.innerHTML = "";
+    console.log("did it wokr");
   } else {
-    const usr_input = input.value.toLowerCase();
-    // console.log(usr_input);
-    const result = await fetch(mov_obj).then((res) => {
-      return res.json();
-    });
-    // console.log(res);
-    let found_items = [],
-      j = 0;
-    for (let i of result.data.movies) {
-      if (i.title.toLowerCase().includes(usr_input)) {
-        found_items[j] = {
-          title: i.title,
-          year: i.year,
-          cover: i.small_cover_image,
-        };
-        j++;
+    setTimeout(async () => {
+      const usr_input = input.value.toLowerCase();
+      const result = await fetch(mov_obj).then((res) => {
+        return res.json();
+      });
+      let found_items = [],
+        j = 0;
+      for (let i of result.data.movies) {
+        if (i.title.toLowerCase().includes(usr_input)) {
+          found_items[j] = {
+            title: i.title,
+            year: i.year,
+            cover: i.small_cover_image,
+          };
+          j++;
+        }
       }
-    }
-    // console.log(found_items);
-    // console.log(result.length);
-    list_box.innerHTML = "";
-    if (found_items.length == 0) {
-      list_box.innerHTML += `<li><span><i>NO RESULTS FOUND!</i></span></li>`;
-    } else {
-      for (let i = 0; i < found_items.length; i++) {
-        list_box.innerHTML += `<li class="animate__animated d-none"><span><img src = '${found_items[i].cover}'><b><i>${found_items[i].year}</i></b></span><span>${found_items[i].title}</span></li>`;
+      list_box.innerHTML = "";
+      if (found_items.length == 0) {
+        list_box.innerHTML += `<li><i>NO RESULTS FOUND!</i></li>`;
+      } else {
+        for (let i = 0; i < found_items.length; i++) {
+          list_box.innerHTML += `
+          <li>
+          <img src = '${found_items[i].cover}'>${found_items[i].title} (${found_items[i].year})
+          </li>`;
+        }
       }
-
-      const list_item = document.querySelectorAll("li");
-      for (let i = 0; i < list_item.length; i++) {
-        setTimeout(() => {
-          list_item[i].classList.remove("d-none");
-          list_item[i].classList.add("animate__fadeInRight");
-        }, 1000 * i);
-      }
-    }
+    }, 1000);
   }
 });
